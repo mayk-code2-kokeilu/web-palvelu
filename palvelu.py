a4a1b914c6c115861f0d9421b51b37df6d9976b7
Panu Kalliokoski <Panu.Kalliokoski@mayk.fi>
2025-03-14 11:50:46 +0200

    painikkeen lisäys

--- palvelu.py
+++ palvelu.py
@@ -13,6 +13,7 @@ def app(environ, respond):
     polku = environ["PATH_INFO"]
     salanimi = polku.replace("a", "aca").replace("i", "hani").replace("n", "nono")
     yield "<p>moikka</p>".encode('utf-8')
+    yield "<form method=GET><input type=button value=paina></form>".encode("utf-8")
     yield (f"Salainen nimesi on: <b>{salanimi}</b>".encode('utf-8'))
     #for key in environ:
     #    yield ("%s: %s\n" % (key, environ[key])).encode('utf-8')
